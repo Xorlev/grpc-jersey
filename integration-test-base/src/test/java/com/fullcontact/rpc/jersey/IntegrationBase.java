1dcda470daec425d9e38db6dd48a61a0d7c6b1cb
Michael Rose <elementation@gmail.com>
2016-12-11 04:06:12 -0700

    integration: create base package; base test

---
+++ integration-test-base/src/test/java/com/fullcontact/rpc/jersey/IntegrationBase.java
@@ -0,0 +1,88 @@
+package com.fullcontact.rpc.jersey;
+
+import com.fullcontact.rpc.NestedType;
+import com.fullcontact.rpc.TestEnum;
+import com.fullcontact.rpc.TestRequest;
+import com.fullcontact.rpc.TestResponse;
+
+import com.google.protobuf.util.JsonFormat;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import javax.ws.rs.client.Entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Base end-to-end Jersey tests
+ *
+ * @author Michael Rose (xorlev)
+ */
+@RunWith(JUnit4.class)
+public abstract class IntegrationBase {
+    public abstract ResourceTestRule resources();
+
+    @Test
+    public void testBasicGet() throws Exception {
+        // /users/{s}/{uint3}/{nt.f1}
+        String responseJson = resources().getJerseyTest()
+                                                 .target("/users/string1/1234/abcd")
+                                                 .request()
+                                                 .buildGet()
+                                                 .invoke(String.class);
+
+        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
+        JsonFormat.parser().merge(responseJson, responseFromJson);
+        TestResponse response = responseFromJson.build();
+
+        assertThat(response.getRequest().getS()).isEqualTo("string1");
+        assertThat(response.getRequest().getUint3()).isEqualTo(1234);
+        assertThat(response.getRequest().getNt().getF1()).isEqualTo("abcd");
+    }
+
+    @Test
+    public void testBasicPost() throws Exception {
+        TestRequest request = TestRequest.newBuilder()
+            .setBoolean(true)
+            .setS("Hello")
+            .setNt(NestedType.newBuilder().setF1("World"))
+            .build();
+        String responseJson = resources().getJerseyTest()
+                 .target("/users/")
+                 .request()
+                 .buildPost(Entity.entity(JsonFormat.printer().print(request), "application/json; charset=utf-8"))
+                 .invoke(String.class);
+
+        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
+        JsonFormat.parser().merge(responseJson, responseFromJson);
+        TestResponse response = responseFromJson.build();
+
+        assertThat(response.getRequest()).isEqualTo(request);
+    }
+
+    @Test
+    public void testAdvancedGet() throws Exception {
+        // /users/{s=hello/**}/x/{uint3}/{nt.f1}/*/**/test
+        String responseJson = resources().getJerseyTest()
+                                       .target("/users/hello/string1/test/x/1234/abcd/foo/bar/baz/test")
+                                       .queryParam("d", 1234.5678)
+                                       .queryParam("enu", "SECOND")
+                                       .queryParam("uint3", "5678") // ensure path param has precedence
+                                       .queryParam("x", "y")
+                                       .request()
+                                       .buildGet()
+                                       .invoke(String.class);
+
+        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
+        JsonFormat.parser().merge(responseJson, responseFromJson);
+        TestResponse response = responseFromJson.build();
+
+        assertThat(response.getRequest().getS()).isEqualTo("hello/string1/test");
+        assertThat(response.getRequest().getUint3()).isEqualTo(1234);
+        assertThat(response.getRequest().getD()).isEqualTo(1234.5678);
+        assertThat(response.getRequest().getEnu()).isEqualTo(TestEnum.SECOND);
+        assertThat(response.getRequest().getNt().getF1()).isEqualTo("abcd");
+    }
+}
--- integration-test-proxy/src/test/java/com/fullcontact/rpc/jersey/ProxyIntegration.java
+++ integration-test-proxy/src/test/java/com/fullcontact/rpc/jersey/ProxyIntegration.java
@@ -1,37 +1,26 @@
 package com.fullcontact.rpc.jersey;
 
-import com.fullcontact.rpc.NestedType;
-import com.fullcontact.rpc.TestEnum;
-import com.fullcontact.rpc.TestRequest;
-import com.fullcontact.rpc.TestResponse;
 import com.fullcontact.rpc.TestServiceGrpc;
 import com.fullcontact.rpc.TestServiceGrpcJerseyResource;
 
-import com.google.protobuf.util.JsonFormat;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import io.grpc.Server;
-import io.grpc.ServerBuilder;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import org.glassfish.jersey.test.jetty.JettyTestContainerFactory;
 import org.junit.ClassRule;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import javax.ws.rs.client.Entity;
-
 import java.io.IOException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
- * End-to-end Jersey tests
+ * Proxy-based end-to-end Jersey tests
  *
  * @author Michael Rose (xorlev)
  */
 @RunWith(JUnit4.class)
-public class ProxyIntegration {
+public class ProxyIntegration extends IntegrationBase {
     private static Server server = InProcessServerBuilder.forName("TestService")
                                                          .addService(new EchoTestService())
                                                          .directExecutor()
@@ -60,65 +49,8 @@ public class ProxyIntegration {
                         .setTestContainerFactory(new JettyTestContainerFactory())
                         .build();
 
-    @Test
-    public void testBasicGet() throws Exception {
-        // /users/{s}/{uint3}/{nt.f1}
-        String responseJson = resources.getJerseyTest()
-                                                 .target("/users/string1/1234/abcd")
-                                                 .request()
-                                                 .buildGet()
-                                                 .invoke(String.class);
-
-        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
-        JsonFormat.parser().merge(responseJson, responseFromJson);
-        TestResponse response = responseFromJson.build();
-
-        assertThat(response.getRequest().getS()).isEqualTo("string1");
-        assertThat(response.getRequest().getUint3()).isEqualTo(1234);
-        assertThat(response.getRequest().getNt().getF1()).isEqualTo("abcd");
-    }
-
-    @Test
-    public void testBasicPost() throws Exception {
-        TestRequest request = TestRequest.newBuilder()
-            .setBoolean(true)
-            .setS("Hello")
-            .setNt(NestedType.newBuilder().setF1("World"))
-            .build();
-        String responseJson = resources.getJerseyTest()
-                 .target("/users/")
-                 .request()
-                 .buildPost(Entity.entity(JsonFormat.printer().print(request), "application/json; charset=utf-8"))
-                 .invoke(String.class);
-
-        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
-        JsonFormat.parser().merge(responseJson, responseFromJson);
-        TestResponse response = responseFromJson.build();
-
-        assertThat(response.getRequest()).isEqualTo(request);
-    }
-
-    @Test
-    public void testAdvancedGet() throws Exception {
-        // /users/{s=hello/**}/x/{uint3}/{nt.f1}/*/**/test
-        String responseJson = resources.getJerseyTest()
-                                       .target("/users/hello/string1/test/x/1234/abcd/foo/bar/baz/test")
-                                       .queryParam("d", 1234.5678)
-                                       .queryParam("enu", "SECOND")
-                                       .queryParam("uint3", "5678") // ensure path param has precedence
-                                       .queryParam("x", "y")
-                                       .request()
-                                       .buildGet()
-                                       .invoke(String.class);
-
-        TestResponse.Builder responseFromJson = TestResponse.newBuilder();
-        JsonFormat.parser().merge(responseJson, responseFromJson);
-        TestResponse response = responseFromJson.build();
-
-        assertThat(response.getRequest().getS()).isEqualTo("hello/string1/test");
-        assertThat(response.getRequest().getUint3()).isEqualTo(1234);
-        assertThat(response.getRequest().getD()).isEqualTo(1234.5678);
-        assertThat(response.getRequest().getEnu()).isEqualTo(TestEnum.SECOND);
-        assertThat(response.getRequest().getNt().getF1()).isEqualTo("abcd");
+    @Override
+    public ResourceTestRule resources() {
+        return resources;
     }
 }
